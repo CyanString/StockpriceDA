2139bd24b2c5d7216dd8586c6a612c8bdfecfda3
Devin Yao <815219292@qq.com>
2025-04-11 02:47:57 -0400

    commit

--- main.py
+++ main.py
@@ -3,12 +3,13 @@ from tkinter import ttk
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from indicator import TrendStrategy
+import numpy as np
 
-#
+#1
 class TradingGUI:
     def __init__(self, root):
         self.root = root
-        self.root.title("quint v1.0")
+        self.root.title("Quint Strategy Model")
         self.setup_ui()
         self.strategy = None
 
@@ -17,10 +18,14 @@ class TradingGUI:
         control_frame = ttk.LabelFrame(self.root, text="parameter setting", padding=10)
         control_frame.pack(fill=tk.X, padx=5, pady=5)
 
-        ttk.Label(control_frame, text="stock ttk").grid(row=0, column=0)
-        self.symbol_entry = ttk.Entry(control_frame)
-        self.symbol_entry.insert(0, "AAPL")
-        self.symbol_entry.grid(row=0, column=1)
+
+        ttk.Label(control_frame, text="Stock token:").grid(row=0, column=0)
+        self.symbol_combo = ttk.Combobox(control_frame,
+                                          values=["AAPL","MSFT","NVDA","GOOGL","AMZN",
+                                                  "META","TSLA","AVGO","ASML","ADBE"])
+        self.symbol_combo.current(0)
+        self.symbol_combo.grid(row=0, column=1)
+
 
         ttk.Label(control_frame, text="time zone").grid(row=1, column=0)
         self.period_combo = ttk.Combobox(control_frame,
@@ -31,36 +36,65 @@ class TradingGUI:
         ttk.Button(control_frame, text="run strategy",
                    command=self.run_strategy).grid(row=2, columnspan=2, pady=10)
 
-        # report
+        # result report
         result_frame = ttk.LabelFrame(self.root, text="result", padding=10)
         result_frame.pack(fill=tk.BOTH, expand=True, padx=5, pady=5)
 
         self.result_text = tk.Text(result_frame, height=8)
         self.result_text.pack(fill=tk.BOTH, expand=True)
 
-        # region
+        # graph region
         self.fig, self.ax = plt.subplots(figsize=(10, 4))
         self.canvas = FigureCanvasTkAgg(self.fig, master=result_frame)
         self.canvas.get_tk_widget().pack(fill=tk.BOTH, expand=True)
 
+        ttk.Label(control_frame, text="TP Multiplier").grid(row=3, column=0)
+        self.tp_entry = ttk.Entry(control_frame)
+        self.tp_entry.insert(0, "1.5")
+        self.tp_entry.grid(row=3, column=1)
+
+        ttk.Label(control_frame, text="SL Multiplier").grid(row=4, column=0)
+        self.sl_entry = ttk.Entry(control_frame)
+        self.sl_entry.insert(0, "0.8")
+        self.sl_entry.grid(row=4, column=1)
+
+
+
+
     def run_strategy(self):
-        symbol = self.symbol_entry.get()
+
+
+        symbol = self.symbol_combo.get()
         period = self.period_combo.get()
 
-        self.strategy = TrendStrategy(symbol=symbol, period=period)
+        try:
+            tp = float(self.tp_entry.get())
+            sl = float(self.sl_entry.get())
+        except ValueError:
+            self.result_text.insert(tk.END, "Error: TP/SL must be numbers!")
+            return
+
+        self.strategy = TrendStrategy(symbol=symbol, period=period,tp_multiplier=tp, sl_multiplier=sl)
         data = self.strategy.run()
+        trades, stats = self.strategy.run_backtest(data)
 
-        self.display_results(data)
-        self.plot_data(data)
+        filename = symbol + "-" + period + ".csv"
+        self.strategy.save_to_csv(data,filename)
 
-    def display_results(self, data):
+        self.display_results(data, stats)
+        self.plot_data(data,trades)
+
+    def display_results(self, data, stats):
         self.result_text.delete(1.0, tk.END)
-        last_signal = data['Buy_Signal'].iloc[-1]
         self.result_text.insert(tk.END,
-                                f"new_signal: {'buy' if last_signal == 1 else 'observe'}\n\n")
-        self.result_text.insert(tk.END, data.tail().to_string())
+                                f"Total Trades: {stats['total_trades']}\n"
+                                f"Win Rate: {stats['win_rate'] * 100:.1f}%\n"
+                                f"Avg Return: {stats['avg_return']:.2f}%\n"
+                                f"Max Win: {stats['max_win']:.2f}%\n"
+                                f"Max Loss: {stats['max_loss']:.2f}%\n"
+                                )
 
-    def plot_data(self, data):
+    def plot_data(self, data,trades):
         self.ax.clear()
         data['Close'].plot(ax=self.ax, label='Price', color='blue')
         data['EMA_Fast'].plot(ax=self.ax, label='EMA_Fast', color='orange')
@@ -70,8 +104,14 @@ class TradingGUI:
         self.ax.scatter(buy_signals.index, buy_signals['Close'],
                         color='red', marker='^', label='buy_signal')
 
+        sell_dates = [t['Date'] for t in trades if t['Action'] == 'Sell']
+        sell_prices = [t['Price'] for t in trades if t['Action'] == 'Sell']
+        self.ax.scatter(sell_dates, sell_prices,
+                        color='black', marker='v', label='Sell Signal')
+
         self.ax.legend()
         self.canvas.draw()
+
 
 
 if __name__ == "__main__":
